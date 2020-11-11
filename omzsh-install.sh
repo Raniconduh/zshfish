2b718a6c950c91b5c28bf7814a02d7a7c985dcb1
Rani <enran@pm.me>
2020-11-11 16:38:19 -0600

    Added installation script

---
+++ omzsh-install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm ~/.oh-my-zsh/custom/themes/zshfish.zsh-theme
+cp zshfish.zsh-theme ~/.oh-my-zsh/custom/themes
+
+echo "Installation finished."
+echo "To see the theme, run \`exec -l \$SHELL\`"
+
