a7e6d28953138288b86a35addaaa2b8a97cd7660
Raniconduh <clagv.randomgames@gmail.com>
2020-11-14 22:56:22 -0600

    Added more verbose installation steps

--- omzsh-install.sh
+++ omzsh-install.sh
@@ -4,5 +4,6 @@ rm ~/.oh-my-zsh/custom/themes/zshfish.zsh-theme
 cp zshfish.zsh-theme ~/.oh-my-zsh/custom/themes
 
 echo "Installation finished."
+echo "To activate theme, edit \`~/.zshrc\` and set ZSH_THEME equal to \"zshfish\"" 
 echo "To see the theme, run \`exec -l \$SHELL\`"
 
