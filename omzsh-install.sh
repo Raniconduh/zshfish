43958372b63247b829f8581d5924a367d5878f6a
Raniconduh <clagv.randomgames@gmail.com>
2020-12-23 09:09:53 -0600

    Add help menu

--- omzsh-install.sh
+++ omzsh-install.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+if [ $1 ]; then
+	echo " Install Script for zshfish theme"
+	echo ""
+	echo " How to use:"
+	echo "   Run this script and zshfish thene will be automativally installed"
+	echo "   Requires omzsh"
+	echo ""
+	echo " Options:"
+	echo "   --help    Display this screen and exit"
+	echo ""
+	exit 0
+fi
+
 rm -f ~/.oh-my-zsh/custom/themes/zshfish.zsh-theme
 cp zshfish.zsh-theme ~/.oh-my-zsh/custom/themes
 
